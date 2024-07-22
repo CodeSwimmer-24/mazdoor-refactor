a7a14d542c2c407e5b73df743c534a14df5d3b3f
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-07-22 19:25:52 +0530

    Removed google login

--- screens/Login/Ui/index.js
+++ screens/Login/Ui/index.js
@@ -1,45 +1,48 @@
 import React, { useState } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { moderateScale } from "react-native-size-matters";
 import Logo from "../../../assets/assets/logo.png";
 import styles from "./styles";
 import colors from "../../../constants/colors";
 
-const LoginUi = ({ onGoogleButtonPress, setUserRole, userRole }) => {
-  const [isVisible, setIsVisible] = useState(false);
+const LoginUi = ({ onLoginPress, setUserRole, userRole }) => {
+  const [email, setEmail] = useState("");
 
-  const handleGoogleButtonPress = (role) => {
-    onGoogleButtonPress();
+  const handleLoginPress = (role) => {
     setUserRole(role);
+    onLoginPress(email);
   };
 
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.logo} />
 
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+
       <View style={styles.buttonContainer}>
         <TouchableOpacity
-          style={styles.googleButton}
-          onPress={() => handleGoogleButtonPress("customer")}
+          style={styles.loginButton}
+          onPress={() => handleLoginPress("customer")}
         >
-          <FontAwesome name="google" size={moderateScale(18)} color="#fff" />
-          <Text style={styles.buttonText}>SignIn with User Account</Text>
+          <Text style={styles.buttonText}>Login as Customer</Text>
         </TouchableOpacity>
       </View>
 
       <View style={[styles.buttonContainer, { marginTop: 25 }]}>
         <TouchableOpacity
-          style={[styles.googleButton, { backgroundColor: "white" }]}
-          onPress={() => handleGoogleButtonPress("mazdoor")}
+          style={[styles.loginButton, { backgroundColor: colors.baseColor }]}
+          onPress={() => handleLoginPress("mazdoor")}
         >
-          <FontAwesome
-            name="google"
-            size={moderateScale(18)}
-            color={colors.baseColor}
-          />
-          <Text style={[styles.buttonText, { color: colors.baseColor }]}>
-            SignIn with Mazdoor Account
+          <Text style={[styles.buttonText, { color: "white" }]}>
+            Login as Mazdoor
           </Text>
         </TouchableOpacity>
       </View>
--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { View, ActivityIndicator } from "react-native";
 import axios from "axios";
-import auth from "@react-native-firebase/auth";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
-
 import LoginUi from "./Ui";
 import RegisterForm from "../Register";
 import Customer from "../Customer";
-
 import { hostUrl, getFavoriteSPs } from "../../services";
 import { useAuthStore } from "../../zustand/authStore";
 import { useCustomerStore } from "../../zustand/customerStore";
@@ -17,24 +13,19 @@ import Mazdoor from "../Mazdoor";
 const Login = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false); // State to manage loading indicator
-  const authStore = useAuthStore(); // Initialize Zustand store
+  const [loading, setLoading] = useState(false);
+  const authStore = useAuthStore();
   const customerStore = useCustomerStore();
 
   const { email, startupApisCalled, setStartupApisCalled } = authStore;
   const { favoriteSps, setFavoriteSps } = customerStore;
 
   const [userRole, setUserRole] = useState("");
-
   const { role } = useAuthStore();
 
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged((user) => {
-      setUser(user);
-      if (initializing) setInitializing(false);
-    });
-
-    return subscriber; // unsubscribe on unmount
+    // Simulate initialization
+    setInitializing(false);
   }, []);
 
   useEffect(() => {
@@ -48,51 +39,31 @@ const Login = () => {
           .catch((err) => console.log(err.response.data));
       }
     }
-  }, [user, email]);
+  }, [user, email, favoriteSps.length, startupApisCalled]);
 
-  GoogleSignin.configure({
-    webClientId:
-      "1061751220739-t2ti12p4u36or9f10qjgk14jrhlt4csn.apps.googleusercontent.com",
-  });
-
-  const onGoogleButtonPress = async () => {
-    setLoading(true); // Set loading to true when starting login process
-
-    await GoogleSignin.hasPlayServices({
-      showPlayServicesUpdateDialog: true,
-    });
-    const { idToken } = await GoogleSignin.signIn();
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  const onLoginPress = async (email) => {
+    setLoading(true);
 
     try {
-      const result = await auth().signInWithCredential(googleCredential);
-      const { displayName, email } = result.user;
-
       const apiUrl = `${hostUrl}/mazdoor/v1/login`;
-
       const response = await axios.post(apiUrl, {
         emailId: email,
         role: userRole,
-        name: displayName,
+        name: "", // Adjust as needed
       });
 
       if (response.status === 200) {
         const responseData = response.data;
-        console.log("User logged in successfully!", responseData);
-
-        authStore.setName(displayName);
         authStore.setEmail(email);
         authStore.setRole(userRole);
-        authStore.setPicture(result.additionalUserInfo.profile.picture);
         authStore.setIsNewUser(responseData.isNewUser);
+
         if (!responseData.isNewUser) {
           const profileApiUrl = `${hostUrl}/mazdoor/v1/getProfile?emailId=${email}`;
           const profileResponse = await axios.get(profileApiUrl);
 
           if (profileResponse.status === 200) {
             const profileData = profileResponse.data;
-            console.log("User profile fetched successfully!");
-
             authStore.setName(profileData.name);
             authStore.setContact(profileData.contactNo);
             authStore.setBuildingAddress(profileData.address.buildingAddress);
@@ -124,7 +95,7 @@ const Login = () => {
         console.error("Error details:", errorData);
       }
     } catch (error) {
-      console.error("Failed to authenticate user with Firebase.", error);
+      console.error("Failed to authenticate user.", error);
     } finally {
       setLoading(false);
     }
@@ -147,7 +118,7 @@ const Login = () => {
   } else if (!user) {
     return (
       <LoginUi
-        onGoogleButtonPress={onGoogleButtonPress}
+        onLoginPress={onLoginPress}
         setUserRole={setUserRole}
         userRole={userRole}
       />
