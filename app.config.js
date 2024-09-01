7d87789150788184073ede99688a04acee8692fb
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-09-01 19:52:49 +0530

    version change

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "Digimazdoor",
     slug: "Digimazdoor",
     plugins: ["@react-native-google-signin/google-signin"],
-    version: "1.1.1",
+    version: "1.1.2",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -15,7 +15,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.fahad999.mazdoorapp",
-      buildNumber: "2.0.0",
+      buildNumber: "3.0.0",
     },
     android: {
       googleServicesFile: process.env.GOOGLE_SERVICES,
@@ -24,14 +24,14 @@ export default {
         backgroundColor: "#ffffff",
       },
       package: "com.fahad999.mazdoorapp",
-      versionCode: 2,
+      versionCode: 3,
     },
     web: {
       favicon: "./assets/favicon.png",
     },
     extra: {
       eas: {
-        projectId: "22b84d92-fefb-4f2c-a4c6-37ac30e85a5a",
+        projectId: "482e2f36-fc4e-4ca9-8561-8d15aa69eb55",
       },
     },
   },
